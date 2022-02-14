00368a0ec973a95ad45bef2317fbf33d8e1ab7c2
Tokhalyan <tokhalyan@gmail.com>
2022-02-14 00:25:22 -0800

    updated looping method

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const profileDataArgs = process.argv.slice(2);
 
-// Notice the lack of parentheses around the `profileDataArr` parameter?
-const printProfileData = (profileDataArr) => {
-    
+const printProfileData = profileDataArr => {
     profileDataArr.forEach(profileItem => console.log(profileItem));
 };
+
 printProfileData(profileDataArgs);
