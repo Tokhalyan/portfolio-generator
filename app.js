f4c20b7e254bd4b658fe5b6a90339636c3a8abdd
Tokhalyan <tokhalyan@gmail.com>
2022-02-17 15:44:21 -0800

    added validations for user answers

--- app.js
+++ app.js
@@ -15,22 +15,56 @@ const promptUser = () => {
         {
             type: 'input',
             name: 'name',
-            message: 'What is your name?'
+            message: 'What is your name? (Required)',
+            validate: nameInput => {
+                if(nameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter your name!");
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'github',
-            message: 'Enter your GitHub username?'
+            message: 'Enter your GitHub username? (Required)',
+            validate: gitInput => {
+                if(gitInput) {
+                    return true;
+                } else {
+                    console.log("Please enter your GitHub username!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: "confirm",
+            name: "confirmAbout",
+            message: 'Would you like to enter some information about yourself for an "About" section?',
+            default: true
         },
         {
             type: 'input',
             name: 'about',
-            message: 'Provide some information about yourself:'
+            message: 'Provide some information about yourself: (Optional)',
+            when: ({confirmAbout}) => {
+                if(confirmAbout) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
         }
     ])
 }
 
 const promptProject = (portfolioData) => {
+    console.log(`
+    =================
+    Add a New Project
+    =================
+    `)
     // If there's no 'projects' array property, create one
     if (!portfolioData.projects) {
         portfolioData.projects = [];
@@ -39,12 +73,28 @@ const promptProject = (portfolioData) => {
         {
             type: 'input',
             name: 'name',
-            message: 'What is the name of your project?'
+            message: 'What is the name of your project? (Required)',
+            validate: projectInput => {
+                if(projectInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the name of your project!");
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'description',
-            message: 'Provide a description of the project (Required)'
+            message: 'Provide a description of the project (Required)',
+            validate: descriptionInput => {
+                if(descriptionInput) {
+                    return true;
+                } else {
+                    console.log("Please write a description for your project!");
+                    return false;
+                }
+            }
         },
         {
             type: 'checkbox',
@@ -55,7 +105,15 @@ const promptProject = (portfolioData) => {
         {
             type: 'input',
             name: 'link',
-            message: 'Enter the GitHub link to your project. (Required)'
+            message: 'Enter the GitHub link to your project. (Required)',
+            validate: linkInput => {
+                if(linkInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the GitHub link to you project!");
+                    return false;
+                }
+            }
         },
         {
             type: 'confirm',
